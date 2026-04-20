5cdee563d5a0086c5f540a4276e09f73b3338ef9
Ebenezer Jesuraj <ebenezerjesuraj@gmail.com>
2026-04-20 12:00:27 +0530

    Enhance model with prediction and summary output
    Added numpy import and updated model summary output. Included prediction for a test company.

--- Pandas-Tensorflow.py
+++ Pandas-Tensorflow.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import tensorflow as tf
+import numpy as np
+
 df = pd.read_csv("fortune1000.csv")
 
 # Select only numerical features for X to make it compatible with a simple Dense layer
@@ -11,6 +13,7 @@ y = df['Employees'].values
 # Define the model before using it
 model = tf.keras.Sequential([
     tf.keras.layers.Dense(64, activation='relu', input_shape=(X.shape[1],)),
+    tf.keras.layers.Dense(32,activation='relu'),
     tf.keras.layers.Dense(1) #Output layer for regression
 ])
 
@@ -18,4 +21,10 @@ model = tf.keras.Sequential([
 model.compile(optimizer='adam', loss='mse')
 
 model.fit(X,y, epochs=10)
-print(model)
+model.summary()
+
+# Rank 10, Revenue 50000, Profit 5000
+test_company = np.array([[10, 50000, 5000]]),
+prediction = model.predict(test_company)
+
+print(f"Predicted Employees: {prediction[0][0]}")
