ee289f032fbe5c6c12682cec19bb3ceb33b3d7bd
Ebenezer Jesuraj <ebenezerjesuraj@gmail.com>
2026-04-28 22:07:14 +0530

    Add data loading and inspection for California housing
    Load California housing data into a pandas DataFrame and print summary information.

---
+++ Pandas/dataLoaderInspector.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+df = pd.read_csv('sample_data/california_housing_train.csv') // Loading data into pandas DataFrame 
+
+print(df.head()) // First 5 rows
+print(df.info()) // Group by Column Non-Null Values and their Dtype
+print(df.describe())  // Statistical summary of the data
