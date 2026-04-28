ecc6486ebf85f333eb93ceea748a78b187c9f102
Ebenezer Jesuraj <ebenezerjesuraj@gmail.com>
2026-04-28 22:11:16 +0530

    Update comments in dataLoaderInspector.py

--- Pandas/dataLoaderInspector.py
+++ Pandas/dataLoaderInspector.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-df = pd.read_csv('sample_data/california_housing_train.csv') // Loading data into pandas DataFrame 
+df = pd.read_csv('sample_data/california_housing_train.csv') # Loading data into pandas DataFrame from csv
 
-print(df.head()) // First 5 rows
-print(df.info()) // Group by Column Non-Null Values and their Dtype
-print(df.describe())  // Statistical summary of the data
+print(df.head()) # First 5 rows
+print(df.info()) # Group by Column Non-Null Values and their Dtype
+print(df.describe())  # Statistical summary of the data
